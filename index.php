6f1e531a82f83854f750c4d332062da213ffd520
Kweku Moses <kweku.moses@hotmail.com>
2022-02-21 16:50:55 +0100

    new styles

--- index.php
+++ index.php
@@ -14,43 +14,72 @@
 
 get_header();
 ?>
+<div id="primary" class="content-area">
+    <main id="main" class="site-main">
 
-<main id="primary" class="site-main">
+        <!-- the hero element  -->
 
-    <?php
+        <div class="hero">
+            <div class="hero-inner container">
+                <h1 class="hero-text">
+                    <span class="hero-sitename">
+                        <?php bloginfo('name'); ?>
+                    </span> Fried Seaweed Rolls
+                </h1>
+
+                <p class="hero-description">
+                    <span class="magenta">
+                        <?php bloginfo('name'); ?>
+                    </span>is a restaurant that creates future flavor nostalgia of street food
+                </p>
+            </div>
+        </div>
+
+        <!-- our intro element -->
+
+        <div class="intro">
+            <div class="intro-inner">
+                <h2 class="intro-title">
+                    Introducing <?php bloginfo('name'); ?>
+                </h2>
+                <p class="intro-description">
+                    street food that was born in tough times. <br>
+                    street food that everybody loves. <br>
+                    <span class="yellow">
+                        <?php bloginfo('name'); ?>
+                    </span> is on a mission to provide future
+                    flavor nostalgia of street food for men,
+                    women, children, grandpas and grandmas.
+                    we only use the best ingredients. 
+                </p>
+            </div>
+        </div>
+
+        <div class="food-title" id="food">
+            Menu
+        </div>
+
+        <div class="food-grid">
+            <?php
 		if ( have_posts() ) :
-
-			if ( is_home() && ! is_front_page() ) :
-				?>
-    <header>
-        <h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-    </header>
-    <?php
-			endif;
-
 			/* Start the Loop */
 			while ( have_posts() ) :
 				the_post();
-
 				/*
 				 * Include the Post-Type-specific template for the content.
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
 				 */
 				get_template_part( 'template-parts/content', get_post_type() );
-
 			endwhile;
-
 			the_posts_navigation();
-
 		else :
-
 			get_template_part( 'template-parts/content', 'none' );
-
 		endif;
 		?>
+        </div>
 
-</main><!-- #main -->
-
+    </main><!-- #main -->
+</div>
 <?php
 get_footer();
